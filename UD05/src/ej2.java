40a6d29d70acb8e38717e4070355057516e59899
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-07 12:13:18 +0200

    done ej2

---
+++ UD05/src/ej2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ej2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("introduce tu nombre: ");
+		String name=sc.nextLine();
+		System.out.println("bienvenid@ "+name);
+	}
+
+}
