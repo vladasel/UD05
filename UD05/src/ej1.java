0caf527acca4826d22e7932b2fbc9765b75c3c07
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-07 12:02:15 +0200

    done ej1

---
+++ UD05/src/ej1.java
@@ -0,0 +1,16 @@
+
+public class ej1 {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 6;
+		if (a > b) {
+			System.out.println(" a es mayor");
+		} else if (a < b) {
+			System.out.println("b es mayor");
+		} else {
+			System.out.println("el valor de las variables es igual");
+		}
+	}
+
+}
