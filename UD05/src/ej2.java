f4f03c055681330217ab31d1196e3f3b6f09361e
ionela vladasel <ionelavladasel2710@gmail.com>
2022-07-07 12:20:09 +0200

    done ej3

--- UD05/src/ej2.java
+++ UD05/src/ej2.java
@@ -1,10 +1,11 @@
-import java.util.*;
+//import java.util.*;
 public class ej2 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("introduce tu nombre: ");
-		String name=sc.nextLine();
+		//Scanner sc = new Scanner(System.in);
+		//System.out.println("introduce tu nombre: ");
+		//String name=sc.nextLine();
+		String name= "ionela";
 		System.out.println("bienvenid@ "+name);
 	}
 
--- UD05/src/ej3.java
+++ UD05/src/ej3.java
@@ -1,9 +1,10 @@
+import javax.swing.JOptionPane;
 
 public class ej3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String name = JOptionPane.showInputDialog("introduce tu nombre");
+		JOptionPane.showMessageDialog(null, "Bienvenid@ " + name);
 	}
 
 }
